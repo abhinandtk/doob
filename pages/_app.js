f8462491c0654f2b55f4f47fc8205d3a5b5ef342
Nibras Khasim <nibras.primalcodes@gmail.com>
2023-03-06 16:07:55 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import '@/styles/Home.scss'
+// import '@/styles/_form.scss'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
