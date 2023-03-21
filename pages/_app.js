26623a9e7487ab4d1327adaefce8d43bd02e8648
AlanMathew098 <alanmanuel1998@gmail.com>
2023-03-21 11:06:48 +0530

    inital commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,13 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '@/styles/Home.scss'
 // import '@/styles/_form.scss'
 
+
+
+
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
+
+  
 }
+
