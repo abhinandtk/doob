2dfa3d124c2117b0dd86969c0ff40f1c8c0b7daf
Nibras Khasim <nibras.primalcodes@gmail.com>
2023-04-01 09:04:25 +0530

    comment reply

--- components/homepage/social/Comments.jsx
+++ components/homepage/social/Comments.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Fragment } from 'react';
-import { useState,useEffect } from 'react'
+import { useState,useEffect,useRef } from 'react'
 import {Modal,Button, CardImg} from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import Axios from 'axios'
@@ -10,6 +10,9 @@ function Comments({setVisibleComment,postId,slug}) {
     const [show,setShow] = useState(true)
     const [comment,setComment] = useState('')
     const [showComments,setShowComments]= useState([])
+    const [replayTo,setReplayTo] = useState(null)
+
+    const inputRef = useRef()
     useEffect(()=>{
 
         Axios.post(apis.commentlist,{
@@ -21,6 +24,8 @@ function Comments({setVisibleComment,postId,slug}) {
             }
         }).then((res)=>{
             setShowComments(res.data.data.comments)
+
+            console.log(res.data.data.comments)
         })
     })
     const onHideHandler =()=>{
@@ -29,12 +34,13 @@ function Comments({setVisibleComment,postId,slug}) {
     }
     const commentPostHandler =(e)=>{
         e.preventDefault()
+        let data = {content: comment, post_id: postId}
+        if (replayTo){
+            data.parent_id=replayTo
+        }
         Axios.post(
             apis.postcomment,
-            {
-              content: comment,
-              post_id: postId
-            },
+            data,
             {
               headers: {
                 'Authorization': `Token ${constants.token_id}`,
@@ -42,10 +48,16 @@ function Comments({setVisibleComment,postId,slug}) {
               }
             }
           ).then((res)=>{
-            // setShowComments(res.data)
+            inputRef.current.value=''
+            setReplayTo(null)
             
           });
     }
+    const handleReply = (commentId) => {
+        setReplayTo(commentId)
+        inputRef.current.focus()
+      }
+    
             
        
     
@@ -75,147 +87,34 @@ function Comments({setVisibleComment,postId,slug}) {
                     {item.content}
                     </p>
                     <p className='small ' style={{color:'#959595',fontWeight:'500',fontSize:'13px'}}>
-                        Reply
+                    <a onClick={() => handleReply(item.id)}>Reply</a>
                     </p>
+                    {item.replies !== null && item.replies.length > 0 ?
+                        item.replies.map((reply) => (
+                            <div className='ms-5 mt-2'>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <p className="mb-0" style={{ fontWeight: '600' }}>
+                                {/* {reply.user.username}{" "} */}
+                                <span className="small" style={{ color: '#959595', fontWeight: '500', fontSize: '13px' }}>4 hrs</span>
+                                </p>
+                            </div>
+                            <p className="small mb-0">
+                                {reply.content}
+                            </p>
+                            </div>
+                        ))
+                        :
+                        ''
+}
+
                 </div>
             </div>
         </div>
         ))}
 
-        <div className='d-flex flex-start mt-4 mx-2'>
-            <div   className='me-2' href=''>
-                <CardImg  className='rounded-circle shadow-1-strong ' src="../images/c2.png" style={{width:'44px',height:'44px'}} ></CardImg>
-            </div>
-            <div className="flex-grow-1 flex-shrink-1 " style={{marginBottom:'-24px'}}>
-                <div>
-                    <div className="d-flex justify-content-between align-items-center">
-                        <p className="mb-0" style={{fontWeight:'600'}}>
-                            Faisal{" "}
-                            <span className="small" style={{color:'#959595',fontWeight:'500',fontSize:'13px'}}>6 hrs</span>
-                        </p>
-                    </div>
-                    <p className="small mb-0">
-                    Lorem Ipsum is simply dummy text
-                    </p>
-                    <p className='small ' style={{color:'#959595',fontWeight:'500',fontSize:'13px'}}>
-                        Reply
-                    </p>
-                </div>
-            </div>
-        </div>
-    
        
-    
-        <div className='d-flex flex-start mt-4 mx-2'>
-            <div className='me-2' href=''>
-                <CardImg  className='rounded-circle shadow-1-strong ' src="../images/c3.png" style={{width:'44px',height:'44px'}} ></CardImg>
-            </div>
-            <div className="flex-grow-1 flex-shrink-1 " style={{marginBottom:'-24px'}}>
-                <div>
-                    <div className="d-flex justify-content-between align-items-center">
-                        <p className="mb-0" style={{fontWeight:'600'}}>
-                            Sulthan Almubarak{" "}
-                            <span className="small" style={{color:'#959595',fontWeight:'500',fontSize:'13px'}}>1 d</span>
-                        </p>
-                    </div>
-                    <p className="small mb-0">
-                    There are many variations of passages
-                    </p>
-                    <p className='small ' style={{color:'#959595',fontWeight:'500',fontSize:'13px'}}>
-                        Reply
-                    </p>
-                </div>
-            </div>
-        </div>
-        
-        <div className='d-flex flex-start mt-4 mx-2'>
-            <div   className='me-2' href=''>
-                <CardImg  className='rounded-circle shadow-1-strong ' src="../images/c4.png" style={{width:'44px',height:'44px'}} ></CardImg>
-            </div>
-            <div className="flex-grow-1 flex-shrink-1 " style={{marginBottom:'-24px'}}>
-                <div>
-                    <div className="d-flex justify-content-between align-items-center">
-                        <p className="mb-0" style={{fontWeight:'600'}}>
-                        Musaid{" "}
-                            <span className="small" style={{color:'#959595',fontWeight:'500',fontSize:'13px'}}>1 d</span>
-                        </p>
-                    </div>
-                    <p className="small mb-0">
-                    It is a long established fact that a reader will be distracted by the readable content
-                    </p>
-                    <p className='small ' style={{color:'#959595',fontWeight:'500',fontSize:'13px'}}>
-                        Reply
-                    </p>  
-                </div>
-            </div>                                                                                                 
-        </div>
-        <div className='d-flex flex-start mt-4 mx-2'>
-            <div   className='me-2' href=''>
-                <CardImg  className='rounded-circle shadow-1-strong ' src="../images/c2.png" style={{width:'44px',height:'44px'}} ></CardImg>
-            </div>
-            <div className="flex-grow-1 flex-shrink-1 " style={{marginBottom:'-24px'}}>
-                <div>
-                    <div className="d-flex justify-content-between align-items-center">
-                        <p className="mb-0" style={{fontWeight:'600'}}>
-                            Faisal{" "}
-                            <span className="small" style={{color:'#959595',fontWeight:'500',fontSize:'13px'}}>6 hrs</span>
-                        </p>
-                    </div>
-                    <p className="small mb-0">
-                    Lorem Ipsum is simply dummy text
-                    </p>
-                    <p className='small ' style={{color:'#959595',fontWeight:'500',fontSize:'13px'}}>
-                        Reply
-                    </p>
-                </div>
-            </div>
-        </div>
-    
-        <div className='d-flex flex-start mt-4 mx-2'>
-            <div className='me-2' href=''>
-                <CardImg  className='rounded-circle shadow-1-strong ' src="../images/c3.png" style={{width:'44px',height:'44px'}} ></CardImg>
-            </div>
-            <div className="flex-grow-1 flex-shrink-1 " style={{marginBottom:'-24px'}}>
-                <div>
-                    <div className="d-flex justify-content-between align-items-center">
-                        <p className="mb-0" style={{fontWeight:'600'}}>
-                            Sulthan Almubarak{" "}
-                            <span className="small" style={{color:'#959595',fontWeight:'500',fontSize:'13px'}}>1 d</span>
-                        </p>
-                    </div>
-                    <p className="small mb-0">
-                    There are many variations of passages
-                    </p>
-                    <p className='small ' style={{color:'#959595',fontWeight:'500',fontSize:'13px'}}>
-                        Reply
-                    </p>
-                </div>
-            </div>
-        </div>
-        
-        <div className='d-flex flex-start mt-4 mx-2'>
-            <div   className='me-2' href=''>
-                <CardImg  className='rounded-circle shadow-1-strong ' src="../images/c4.png" style={{width:'44px',height:'44px'}} ></CardImg>
-            </div>
-            <div className="flex-grow-1 flex-shrink-1 " style={{marginBottom:'-24px'}}>
-                <div>
-                    <div className="d-flex justify-content-between align-items-center">
-                        <p className="mb-0" style={{fontWeight:'600'}}>
-                        Musaid{" "}
-                            <span className="small" style={{color:'#959595',fontWeight:'500',fontSize:'13px'}}>1 d</span>
-                        </p>
-                    </div>
-                    <p className="small mb-0">
-                    It is a long established fact that a reader will be distracted by the readable content
-                    </p>
-                    <p className='small ' style={{color:'#959595',fontWeight:'500',fontSize:'13px'}}>
-                        Reply
-                    </p>  
-                </div>
-            </div>                                                                                                 
-        </div>
-        
-        <div className='d-flex flex-start mt-5 mx-2'>
+        </Modal.Body>
+        <div className='d-flex flex-start mt-5 mx-2' style={{position:'sticky',bottom:'0'}}>
             <div   className='me-2' href=''>
                 <CardImg  className='rounded-circle shadow-1-strong ' src="../images/c8.png" style={{width:'44px',height:'44px'}} ></CardImg>
             </div>
@@ -227,7 +126,8 @@ function Comments({setVisibleComment,postId,slug}) {
                                 <Form.Control type="" 
                                 placeholder="Add a comment" 
                                 className='mark' style={{fontSize:'13px',height:'37px'}} 
-                                onChange={(e)=>setComment(e.target.value)}/> 
+                                onChange={(e)=>setComment(e.target.value)}
+                                ref={inputRef}/> 
                             </Form.Group>  
                             <p  
                             className='mx-2'
@@ -242,7 +142,6 @@ function Comments({setVisibleComment,postId,slug}) {
             </div>
         </div>
 
-        </Modal.Body>
     </Modal>
     </Fragment>
   )
