1d164151fabffec20db3c5a883d3a5ae1d43cba8
AlanMathew098 <alanmanuel1998@gmail.com>
2023-05-04 12:57:33 +0530

    toymm

--- pages/tournament/Book-Game.jsx
+++ pages/tournament/Book-Game.jsx
@@ -298,9 +298,43 @@ Arabic</Dropdown.Item>
 
 <div className="tour-container">
 <form className="nosubmit ">
-<span> <input className="nosubmit1" type="search" placeholder="Search"   /><img src='../images/store/Fil-icon.png' className='filters-icon'></img></span>
+<span> <input className="nosubmit1" type="search" placeholder="Search"   />
+<img src='../images/tournament/Fil-icon.png' className='filters-icon'></img></span>
 </form> 
-
+<div id="carouselExampleIndicators" class="carousel slide my-2">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 5"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../images/tournament/soccer-players-action-professional-stadium 2.png" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
 <div className='row game'>
 <div className='col-md-6'>
