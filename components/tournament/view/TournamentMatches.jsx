d7fda5e028e3309cf833c8895264cf34a907e310
AlanMathew098 <alanmanuel1998@gmail.com>
2023-05-29 13:21:59 +0530

    tt

--- components/tournament/view/TournamentMatches.jsx
+++ components/tournament/view/TournamentMatches.jsx
@@ -371,16 +371,20 @@ function TournamentMatches({ data, setOnSuccess, admin }) {
           value={matchTime}
         />
       </Modal>
+     
       {data.length >= 1 ? (
         data.map((item, index) => (
           <>
-            <h6
+          
+         <h6
               key={index}
               className="my-4"
               style={{ fontSize: "15px", fontWeight: "600" }}
             >
               {item.match_type}
-            </h6>
+              <span className="quaters"> <button className="adds-btn ms-auto">Add</button></span>  </h6>
+           
+           
            
             {item.matches.map((content, index_) => (
               <div key={index_} className="card football1">
