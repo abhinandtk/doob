4711348dac1af89d5f71b60bc75499a656dc6789
Nibras Khasim <nibras.primalcodes@gmail.com>
2023-08-16 10:47:13 +0530

    chatdesign fix

--- components/chat/header/ChatBoxHeader.jsx
+++ components/chat/header/ChatBoxHeader.jsx
@@ -3,8 +3,18 @@ import { Dropdown } from "react-bootstrap";
 import ChatHeaderActions from "./ChatHeaderActions";
 import constants from "@/public/data/my-constants/Constants";
 import ChatHeaderActionsGroup from "./ChatHeaderActionsGroup";
+import moment from "moment";
 
 function ChatBoxHeader({ details, selectedId, setOnSuccess, onNewMsg }) {
+  console.log("trtrtrtrtrweee", details);
+  function lastSeenHandler(lastLoginTime) {
+    const currentTime = moment();
+    const loginTime = moment(lastLoginTime);
+
+    const timeDiff = currentTime.diff(loginTime, "minutes");
+
+    return timeDiff <= 3;
+  }
   return (
     <Fragment>
       <div className="header-full-chat">
@@ -45,38 +55,41 @@ function ChatBoxHeader({ details, selectedId, setOnSuccess, onNewMsg }) {
                 >
                   {details.name}
                   <br></br>
-                  <span
-                    style={{
-                      fontWeight: "400",
-                      fontSize: "13px",
-                      color: "#797C7B",
-                    }}
-                  >
-                    Active Now
-                  </span>
+                  {lastSeenHandler(details.last_login) && details.type!=="group"&&(
+                    <span
+                      style={{
+                        fontWeight: "400",
+                        fontSize: "13px",
+                        color: "#797C7B",
+                      }}
+                    >
+                      Active Now
+                    </span>
+                  )}
                 </p>
               </div>
-              <svg
-                width="10"
-                height="10"
-                style={{
-                 
-                  marginTop: "63px",
-                  left:"70px",
-                 position:"absolute"
-                }}
-                viewBox="0 0 10 10"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <ellipse
-                  cx="4.82459"
-                  cy="5.16357"
-                  rx="4.66443"
-                  ry="4.83643"
-                  fill="#17A803"
-                />
-              </svg>
+              {lastSeenHandler(details.last_login) && details.type!=="group"&&(
+                <svg
+                  width="10"
+                  height="10"
+                  style={{
+                    marginTop: "63px",
+                    left: "70px",
+                    position: "absolute",
+                  }}
+                  viewBox="0 0 10 10"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <ellipse
+                    cx="4.82459"
+                    cy="5.16357"
+                    rx="4.66443"
+                    ry="4.83643"
+                    fill="#17A803"
+                  />
+                </svg>
+              )}
             </div>
             {details.type === "group" ? (
               <ChatHeaderActionsGroup
