e6d2267abff1d176e91c41a7184424a8a0bedb44
Nibras Khasim <nibras.primalcodes@gmail.com>
2023-09-18 11:19:27 +0530

    story

--- pages/story-view/[storyId].jsx
+++ pages/story-view/[storyId].jsx
@@ -1,17 +1,12 @@
 import React, { Fragment, useEffect, useState } from "react";
-
 import axios from "axios";
 import constants from "@/public/data/my-constants/Constants";
 import apis from "@/public/data/my-constants/Apis";
-
 import MainHeader from "@/components/shared/headers/MainHeader";
 import MobileHeader from "@/components/MobileHeader";
 import MainSidebarFixed from "@/components/shared/sidebar/MainSidebarFixed";
-import SingleContainerHomePosts from "@/components/homepage/SingleContainerHomePosts";
-import StoriesMainPage from "@/components/homepage/StoriesMainPage";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Stories, { WithSeeMore } from "react-insta-stories";
-import { Avatar, Card, List } from "antd";
 import Axios from "axios";
 import moment from "moment";
 import { useRouter } from "next/router";
@@ -29,7 +24,9 @@ function StoriesView() {
   const router = useRouter();
   const { storyId } = router.query;
 
-  const [currentStoryIndex, setCurrentStoryIndex] = useState(storyId);
+  const [currentStoryIndex, setCurrentStoryIndex] = useState(parseInt(storyId));
+  console.log("storyId", storyId, currentStoryIndex);
+
   useEffect(() => {
     axios.post(apis.country).then((res) => {
       setCountryData(res.data.country);
@@ -55,32 +52,28 @@ function StoriesView() {
   }
 
   function handleStoryEnd() {
-    // Check if there are more stories
     if (currentStoryIndex < storyList.length - 1) {
-      // Increment the current story index
       setCurrentStoryIndex(currentStoryIndex + 1);
     } else {
-      // All stories have ended, you can handle this case here
-      console.log("All stories have ended.");
+      router.push("/");
     }
   }
   let formattedStoryList;
-  // Render stories based on the current story index
-  if (storyList) {
-    formattedStoryList = storyList.map((userItem) => {
-      const userStories = userItem.story.map((storyData) => ({
-        url: `${constants.port}${storyData.image}`,
-        duration: 5000,
-        header: {
-          heading: userItem.user.name,
-          subheading: `Posted ${timeSincePost(storyData.posted)}`,
-          profileImage: `${constants.port}/media/${userItem.user.image}`,
-        },
-      }));
+  // if (storyList) {
+  formattedStoryList = storyList.map((userItem) => {
+    const userStories = userItem.story.map((storyData) => ({
+      url: `${constants.port}${storyData.image}`,
+      duration: 5000,
+      header: {
+        heading: userItem.user.name,
+        subheading: `Posted ${timeSincePost(storyData.posted)}`,
+        profileImage: `${constants.port}/media/${userItem.user.image}`,
+      },
+    }));
 
-      return { story: userStories };
-    });
-  }
+    return { story: userStories };
+  });
+  // }
 
   const currentStory = formattedStoryList[currentStoryIndex];
 
@@ -93,22 +86,18 @@ function StoriesView() {
         <section className="content-container">
           <div className="content">
             {/* <StoriesMainPage /> */}
-            {formattedStoryList.length > 0 && currentStory && (
+            {currentStory && (
               <Stories
                 loop={true}
                 stories={currentStory.story}
                 progressStyles={false}
-                onAllStoriesEnd={() =>
-                  setCurrentStoryIndex(currentStoryIndex + 1)
-                }
+                onAllStoriesEnd={handleStoryEnd}
                 // onStoryStart={(s, st) => console.log("story started", s, st)}
                 // defaultInterval={1500}
                 // width={432}
                 // height={768}
               />
             )}
-
-            {/* <SingleContainerHomePosts story={true} /> */}
           </div>
         </section>
       </main>
